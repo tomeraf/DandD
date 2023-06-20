106c32abad2c1b9b9aad98c24e5d489cf949b3fb
TomerAF <tomeraf@post.bgu.ac.il>
2023-06-20 23:06:52 +0300

    warrior test

--- src/Tests/WarriorTests.java
+++ src/Tests/WarriorTests.java
@@ -7,6 +7,8 @@ import java.util.LinkedList;
 public class WarriorTests {
     private Warrior warrior;
     private Warrior warrior2;
+    private Warrior warrior3;
+
     private Monster monster1;
 
     private Monster monster2;
@@ -24,7 +26,10 @@ public class WarriorTests {
         monster3 = new Monster(10, 10, 'm',0, 1,1,1,1,"monster3");
         warrior = new Warrior(2, 2,10, 100,100, 100,"warrior");
         warrior.SetEXP(50);
-        warrior2 = new Warrior(2, 2,10, 100,100, 100,"warrior2");
+        warrior2 = new Warrior(2, 2,10, 130,112, 104,"warrior2");
+        warrior2.SetEXP(50);
+        warrior3 = new Warrior(2, 2,10, 130,112, 104,"warrior3");
+        warrior3.SetLVL(2);
         powerlist = new LinkedList<>();
         powerlist.add(monster1);
         warrior.powerRefresh(powerlist);
@@ -32,20 +37,26 @@ public class WarriorTests {
     }
 
     @Test
-    void castAbilityTest(){Assert.assertEquals("expected true",warrior.castAbility(),CastPair);}
+    public void castAbilityTest(){
+        Pair<LinkedList<Enemy>,String> WCast = warrior.castAbility();
+        Assert.assertEquals("power list should have monster1,expected true",CastPair.first(),WCast.first());
+        Assert.assertEquals("String should be GO GO Avenger’s Shield!,expected true",CastPair.second(),WCast.second());
+
+    }
     @Test
-    void equalsTest(){Assert.assertEquals("expected true",warrior,warrior2);}
+    public void equalsTest(){Assert.assertEquals("expected true",warrior2,warrior);}
     @Test
-    void lvlupTest(){Assert.assertEquals("expected true",warrior.LVLUP(),warrior2);}
+    public void lvlupTest(){warrior.LVLUP();Assert.assertEquals("expected true",warrior3,warrior);}
     @Test
-    void tickTest(){
+    public void tickTest(){
         powerlist = new LinkedList<>();
         powerlist.addFirst(monster2);
         powerlist.addFirst(monster3);
-        warrior.tick(powerlist);
+        warrior3.SetResourceRemaining(3);
+        warrior3.tick(powerlist);
         powerlist.removeFirst();
-        Assert.assertEquals("expected true",warrior.GetResourceRemaining(),2);
-        Assert.assertEquals("expected true",warrior.GetPower(),powerlist);
+        Assert.assertEquals("expected true",2,warrior3.GetResourceRemaining());
+        Assert.assertEquals("expected true",powerlist,warrior3.GetPower());
     }
 
 
